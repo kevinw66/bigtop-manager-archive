474e02fd6dc97820964bd6a1c9d7f7e97d2fcba4
Zhiguo Wu <wuzhiguo@apache.org>
2023-04-10 22:18:55 +0800

    server-agent websocket

---
+++ bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/configuration/WebSocketConfiguration.java
@@ -0,0 +1,14 @@
+package org.apache.bigtop.manager.server.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+public class WebSocketConfiguration {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
