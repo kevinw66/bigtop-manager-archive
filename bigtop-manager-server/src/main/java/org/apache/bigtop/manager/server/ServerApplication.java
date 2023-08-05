0205753ad41cf926b8025d79805e56be9f5f94bf
Yu Hou <524860213@qq.com>
2023-08-05 15:45:28 +0800

    [feature] Enable Auditing for Spring Data JPA

--- bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/ServerApplication.java
+++ bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/ServerApplication.java
@@ -2,7 +2,9 @@ package org.apache.bigtop.manager.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication(scanBasePackages = "org.apache.bigtop.manager")
 public class ServerApplication {
 
--- bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/orm/entity/BaseEntity.java
+++ bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/orm/entity/BaseEntity.java
@@ -1,16 +1,23 @@
 package org.apache.bigtop.manager.server.orm.entity;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.sql.Timestamp;
 
 @Data
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
+    @CreatedDate
     private Timestamp createTime;
 
+    @LastModifiedDate
     private Timestamp updateTime;
 
 }
