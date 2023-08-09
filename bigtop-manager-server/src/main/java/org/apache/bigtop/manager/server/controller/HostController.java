ace9f22a84ecf409043fdfa3d89e34cb7d2fb26a
Yu Hou <524860213@qq.com>
2023-08-09 15:32:26 +0800

    [improvement] Optimize components to adapt to configuration cache (#33)

--- bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/controller/HostController.java
+++ bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/controller/HostController.java
@@ -1,17 +1,26 @@
 package org.apache.bigtop.manager.server.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+import org.apache.bigtop.manager.common.utils.JsonUtils;
+import org.apache.bigtop.manager.common.utils.stack.StackConfigUtils;
 import org.apache.bigtop.manager.server.model.dto.HostDTO;
 import org.apache.bigtop.manager.server.model.mapper.HostMapper;
 import org.apache.bigtop.manager.server.model.request.HostRequest;
 import org.apache.bigtop.manager.server.model.vo.HostVO;
+import org.apache.bigtop.manager.server.orm.entity.ServiceConfig;
+import org.apache.bigtop.manager.server.orm.entity.Stack;
+import org.apache.bigtop.manager.server.orm.repository.ClusterRepository;
+import org.apache.bigtop.manager.server.orm.repository.ServiceConfigRepository;
+import org.apache.bigtop.manager.server.orm.repository.ServiceRepository;
 import org.apache.bigtop.manager.server.service.HostService;
 import org.apache.bigtop.manager.server.utils.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Tag(name = "Host Controller")
 @RestController
@@ -57,6 +66,41 @@ public class HostController {
     @GetMapping("/cache")
     public void cache(@RequestParam Long clusterId) {
         hostService.cache(clusterId);
+    }
+
+    @Resource
+    private ServiceConfigRepository serviceConfigRepository;
+    @Resource
+    private ClusterRepository clusterRepository;
+    @Resource
+    private ServiceRepository serviceRepository;
+
+    @Operation(summary = "test", description = "test")
+    @GetMapping("/test")
+    public void test() throws JsonProcessingException {
+        Map<String, Object> loadedConfig = StackConfigUtils.loadConfig(this.getClass().getClassLoader()
+                .getResource("stacks/BIGTOP/3.2.0/services/ZOOKEEPER/configuration/zoo.cfg.yaml").getPath());
+
+        ServiceConfig serviceConfig = new ServiceConfig();
+        serviceConfig.setVersion(1);
+        serviceConfig.setTypeName("zoo.cfg");
+        serviceConfig.setConfigData(JsonUtils.OBJECTMAPPER.writeValueAsString(loadedConfig));
+        clusterRepository.findByClusterName("c1").ifPresent(serviceConfig::setCluster);
+        serviceRepository.findByServiceName("zookeeper").ifPresent(serviceConfig::setService);
+
+        serviceConfigRepository.save(serviceConfig);
+
+        Map<String, Object> loadedConfig2 = StackConfigUtils.loadConfig(this.getClass().getClassLoader()
+                .getResource("stacks/BIGTOP/3.2.0/services/ZOOKEEPER/configuration/zookeeper-env.yaml").getPath());
+
+        ServiceConfig serviceConfig2 = new ServiceConfig();
+        serviceConfig2.setVersion(1);
+        serviceConfig2.setTypeName("zookeeper-env");
+        serviceConfig2.setConfigData(JsonUtils.OBJECTMAPPER.writeValueAsString(loadedConfig2));
+        clusterRepository.findByClusterName("c1").ifPresent(serviceConfig2::setCluster);
+        serviceRepository.findByServiceName("zookeeper").ifPresent(serviceConfig2::setService);
+
+        serviceConfigRepository.save(serviceConfig2);
     }
 
 }
--- bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/orm/repository/ServiceRepository.java
+++ bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/orm/repository/ServiceRepository.java
@@ -4,8 +4,11 @@ import org.apache.bigtop.manager.server.orm.entity.Service;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ServiceRepository extends CrudRepository<Service, Long> {
 
     List<Service> findAllByClusterId(Long clusterId);
+
+    Optional<Service> findByServiceName(String serviceName);
 }
