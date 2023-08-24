f38116b90c2749f81821be6085527179bcc5ecd2
Zhiguo Wu <wuzhiguo@apache.org>
2023-08-24 11:26:23 +0800

    编译优化

--- bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/configuration/WebConfiguration.java
+++ bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/configuration/WebConfiguration.java
@@ -9,22 +9,16 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Enumeration;
-import java.util.regex.Pattern;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
-import java.util.zip.ZipFile;
-
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Resource
     private AuthInterceptor authInterceptor;
+
+    private static final String API_PREFIX = "/api";
+
+    private static final String PREFIXED_PACKAGE = "org.apache.bigtop.manager.server.controller";
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -40,93 +34,17 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
-        configurer.addPathPrefix("/api", c -> c.getPackageName().equals("org.apache.bigtop.manager.server.controller"));
+        configurer.addPathPrefix(API_PREFIX, c -> c.getPackageName().equals(PREFIXED_PACKAGE));
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/ui/**").addResourceLocations("classpath:/ui/");
+        registry.addResourceHandler("/ui/**").addResourceLocations("file:ui/");
     }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("redirect:/ui/");
         registry.addViewController("/ui/").setViewName("forward:/ui/index.html");
-    }
-
-    public static Collection<String> getResources(
-            final Pattern pattern){
-        final ArrayList<String> retval = new ArrayList<String>();
-        final String classPath = System.getProperty("java.class.path", ".");
-        final String[] classPathElements = classPath.split(System.getProperty("path.separator"));
-        for(final String element : classPathElements){
-            retval.addAll(getResources(element, pattern));
-        }
-        return retval;
-    }
-
-    private static Collection<String> getResources(
-            final String element,
-            final Pattern pattern){
-        final ArrayList<String> retval = new ArrayList<String>();
-        final File file = new File(element);
-        if(file.isDirectory()){
-            retval.addAll(getResourcesFromDirectory(file, pattern));
-        } else{
-            retval.addAll(getResourcesFromJarFile(file, pattern));
-        }
-        return retval;
-    }
-
-    private static Collection<String> getResourcesFromJarFile(
-            final File file,
-            final Pattern pattern){
-        final ArrayList<String> retval = new ArrayList<String>();
-        ZipFile zf;
-        try{
-            zf = new ZipFile(file);
-        } catch(final ZipException e){
-            throw new Error(e);
-        } catch(final IOException e){
-            throw new Error(e);
-        }
-        final Enumeration e = zf.entries();
-        while(e.hasMoreElements()){
-            final ZipEntry ze = (ZipEntry) e.nextElement();
-            final String fileName = ze.getName();
-            final boolean accept = pattern.matcher(fileName).matches();
-            if(accept){
-                retval.add(fileName);
-            }
-        }
-        try{
-            zf.close();
-        } catch(final IOException e1){
-            throw new Error(e1);
-        }
-        return retval;
-    }
-
-    private static Collection<String> getResourcesFromDirectory(
-            final File directory,
-            final Pattern pattern){
-        final ArrayList<String> retval = new ArrayList<String>();
-        final File[] fileList = directory.listFiles();
-        for(final File file : fileList){
-            if(file.isDirectory()){
-                retval.addAll(getResourcesFromDirectory(file, pattern));
-            } else{
-                try{
-                    final String fileName = file.getCanonicalPath();
-                    final boolean accept = pattern.matcher(fileName).matches();
-                    if(accept){
-                        retval.add(fileName);
-                    }
-                } catch(final IOException e){
-                    throw new Error(e);
-                }
-            }
-        }
-        return retval;
     }
 }
