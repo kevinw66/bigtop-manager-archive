e2d3465b96776c79a4f2bceeebaa919c91ea1b2f
Kengo Seki <sekikn@apache.org>
2023-10-08 10:35:48 +0900

    [fix] (server) Fix o.a.b.manager.server.ServerApplicationTests to work (#71)

--- bigtop-manager-server/src/test/java/org/apache/bigtop/manager/server/ServerApplicationTests.java
+++ bigtop-manager-server/src/test/java/org/apache/bigtop/manager/server/ServerApplicationTests.java
@@ -3,7 +3,7 @@ package org.apache.bigtop.manager.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ServerApplicationTests {
 
     @Test
