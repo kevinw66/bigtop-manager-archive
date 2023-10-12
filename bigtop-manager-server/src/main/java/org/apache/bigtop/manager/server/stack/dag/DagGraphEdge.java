3f928be5538a12fd646f6a1f282aba32b62549e3
Zhiguo Wu <wuzhiguo@apache.org>
2023-10-12 10:19:42 +0800

    edge tostring

--- bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/stack/dag/DagGraphEdge.java
+++ bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/stack/dag/DagGraphEdge.java
@@ -13,6 +13,6 @@ public class DagGraphEdge {
 
     @Override
     public String toString() {
-        return startNode + " --(requires)--> " + endNode;
+        return startNode + " -> " + endNode;
     }
 }
