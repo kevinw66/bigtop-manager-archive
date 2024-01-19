fcd518a5db74b970b32b91f5446f1d8d883b0522
Zhiguo Wu <wuzhiguo@apache.org>
2024-01-19 12:29:31 +0800

    duplicated equals and hashcode

--- bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/job/CommandIdentifier.java
+++ bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/job/CommandIdentifier.java
@@ -6,8 +6,6 @@ import lombok.EqualsAndHashCode;
 import org.apache.bigtop.manager.common.enums.Command;
 import org.apache.bigtop.manager.server.enums.CommandLevel;
 
-import java.util.Objects;
-
 @Data
 @EqualsAndHashCode
 @AllArgsConstructor
@@ -16,17 +14,4 @@ public class CommandIdentifier {
     private CommandLevel commandLevel;
 
     private Command command;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CommandIdentifier that = (CommandIdentifier) o;
-        return commandLevel == that.commandLevel && command == that.command;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(commandLevel, command);
-    }
 }
