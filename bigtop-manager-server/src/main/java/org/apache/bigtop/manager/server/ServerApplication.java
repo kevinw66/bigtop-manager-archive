9e8ffc045276ad6ddd9fe0bb4957f28d67ede53a
Yu Hou <524860213@qq.com>
2024-03-11 02:04:28 +0000

    !25 [Bug] (server) 修复切换到hibernate后不会自动创建表结构的问题
    From: @timyuer 
    Reviewed-by: @kevinw66 
    Signed-off-by: @kevinw66

--- bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/ServerApplication.java
+++ bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/ServerApplication.java
@@ -2,6 +2,7 @@ package org.apache.bigtop.manager.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAsync
 @EnableJpaAuditing
+@EntityScan("org.apache.bigtop.manager.dao")
 @SpringBootApplication(scanBasePackages = {"org.apache.bigtop.manager.server", "org.apache.bigtop.manager.common"})
 public class ServerApplication {
 
