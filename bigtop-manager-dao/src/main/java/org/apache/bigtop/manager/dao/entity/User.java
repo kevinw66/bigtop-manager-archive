c8224e0826dd723aeb2c9e51dbc517dd049b8f42
lvkaihua <215957054@qq.com>
2024-03-11 10:05:10 +0000

    !32 [Bug] (server) Missing import for enableJpaRepositories
    From: @lvkaihua 
    Reviewed-by: @kevinw66 
    Signed-off-by: @kevinw66

--- bigtop-manager-dao/src/main/java/org/apache/bigtop/manager/dao/entity/User.java
+++ bigtop-manager-dao/src/main/java/org/apache/bigtop/manager/dao/entity/User.java
@@ -6,7 +6,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = true)
 @Entity
-@Table(name = "user")
+@Table(name = "\"user\"")
 @TableGenerator(name = "user_generator", table = "sequence", pkColumnName = "seq_name", valueColumnName = "seq_count")
 public class User extends BaseEntity {
 
--- bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/ServerApplication.java
+++ bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/ServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
