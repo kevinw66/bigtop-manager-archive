f9612c7ddb63ba39e38d72acd244c57d6ace033e
吴治国 <wzg547228197@163.com>
2024-04-09 06:04:42 +0000

    !48 [Bug] (license) Fix license error
    From: @kevinw66 
    Reviewed-by: @macchen1 
    Signed-off-by: @macchen1

--- bigtop-manager-ui/src/components/job-info/custom-progress.vue
+++ bigtop-manager-ui/src/components/job-info/custom-progress.vue
@@ -1,12 +1,21 @@
-<template>
-  <div>
-    <a-progress
-      :percent="percent"
-      :status="status"
-      :stroke-color="State[props.state]"
-    />
-  </div>
-</template>
+<!--
+  - Licensed to the Apache Software Foundation (ASF) under one
+  - or more contributor license agreements.  See the NOTICE file
+  - distributed with this work for additional information
+  - regarding copyright ownership.  The ASF licenses this file
+  - to you under the Apache License, Version 2.0 (the
+  - "License"); you may not use this file except in compliance
+  - with the License.  You may obtain a copy of the License at
+  -
+  -    https://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing,
+  - software distributed under the License is distributed on an
+  - "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  - KIND, either express or implied.  See the License for the
+  - specific language governing permissions and limitations
+  - under the License.
+  -->
 
 <script lang="ts" setup>
   import { computed, ref, reactive, watch, toRefs } from 'vue'
@@ -80,5 +89,15 @@
     }
   )
 </script>
+
+<template>
+  <div>
+    <a-progress
+      :percent="percent"
+      :status="status"
+      :stroke-color="State[props.state]"
+    />
+  </div>
+</template>
 
 <style lang="scss" scoped></style>
--- bigtop-manager-ui/src/components/job-info/stage-info.vue
+++ bigtop-manager-ui/src/components/job-info/stage-info.vue
@@ -1,31 +1,21 @@
-<template>
-  <div class="stage-info">
-    <a-table
-      :pagination="paginationProps"
-      :data-source="pagedList"
-      :columns="props.columns"
-      :scroll="{ y: 500 }"
-    >
-      <template #headerCell="{ column }">
-        <span>{{ $t(column.title) }}</span>
-      </template>
-      <template #bodyCell="{ column, text, record }">
-        <template v-if="column.dataIndex === 'name'">
-          <a @click="clickStage(record)">
-            {{ text }}
-          </a>
-        </template>
-        <template v-if="column.dataIndex === 'state'">
-          <custom-progress
-            :key="record.id"
-            :state="text"
-            :progress="record.tasks"
-          />
-        </template>
-      </template>
-    </a-table>
-  </div>
-</template>
+<!--
+  - Licensed to the Apache Software Foundation (ASF) under one
+  - or more contributor license agreements.  See the NOTICE file
+  - distributed with this work for additional information
+  - regarding copyright ownership.  The ASF licenses this file
+  - to you under the Apache License, Version 2.0 (the
+  - "License"); you may not use this file except in compliance
+  - with the License.  You may obtain a copy of the License at
+  -
+  -    https://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing,
+  - software distributed under the License is distributed on an
+  - "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  - KIND, either express or implied.  See the License for the
+  - specific language governing permissions and limitations
+  - under the License.
+  -->
 
 <script lang="ts" setup>
   import { StageVO } from '@/api/job/types.ts'
@@ -66,5 +56,34 @@
     emits('clickStage', record)
   }
 </script>
+
+<template>
+  <div class="stage-info">
+    <a-table
+      :pagination="paginationProps"
+      :data-source="pagedList"
+      :columns="props.columns"
+      :scroll="{ y: 500 }"
+    >
+      <template #headerCell="{ column }">
+        <span>{{ $t(column.title) }}</span>
+      </template>
+      <template #bodyCell="{ column, text, record }">
+        <template v-if="column.dataIndex === 'name'">
+          <a @click="clickStage(record)">
+            {{ text }}
+          </a>
+        </template>
+        <template v-if="column.dataIndex === 'state'">
+          <custom-progress
+            :key="record.id"
+            :state="text"
+            :progress="record.tasks"
+          />
+        </template>
+      </template>
+    </a-table>
+  </div>
+</template>
 
 <style lang="scss" scoped></style>
